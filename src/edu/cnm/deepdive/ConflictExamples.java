f0a64c5c7cd6f33c986c45057c178f584b2ea850
Jan Gevaert <jan.gevaert@gmail.com>
2021-07-04 10:25:23 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/ConflictExamples.java
@@ -0,0 +1,18 @@
+package edu.cnm.deepdive;
+
+import java.util.Date;
+
+public class ConflictExamples {
+
+  public static void main(String[] args) {
+    // write it with the full class name: java.util.Date date;
+    //or import as below. See line 3.
+
+    Date date;
+    //if we want to import java.sql.Date, we need to write the full class name as below:
+    java.sql.Date sqlDate;
+
+
+  }
+
+}
